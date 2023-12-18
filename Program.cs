58ee91fa392a094622bb8decb201f694bbbb5e88
Gabiroba <gabrielcardosogirarde@gmail.com>
2023-12-18 14:34:58 -0300

    First commit

--- Program.cs
+++ Program.cs
@@ -1,6 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorPages();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseRouting();
+app.MapRazorPages();
 
 app.Run();
